849ef0bce131b5070594c832f37932d5649764e2
HaNoWSss <83452795+HaNoWSss@users.noreply.github.com>
2024-12-11 23:38:11 +0200

    ModifiedWoodcarverCityViewAndWoodcarverCreateViewModels

---
+++ WoodcarvingApp.Web/WoodcarvingApp.Web.ViewModels/Woodcarver/CityViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WoodcarvingApp.Web.ViewModels.Woodcarver
+{
+    public class CityViewModel
+    {
+        public Guid Id { get; set; }
+        public string CityName { get; set; } = null!;
+    }
+}
--- WoodcarvingApp.Web/WoodcarvingApp.Web.ViewModels/Woodcarver/WoodcarverCreateViewModel.cs
+++ WoodcarvingApp.Web/WoodcarvingApp.Web.ViewModels/Woodcarver/WoodcarverCreateViewModel.cs
@@ -1,18 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using static WoodcarvingApp.Common.ApplicationConstants;
+
 
 namespace WoodcarvingApp.Web.ViewModels.Woodcarver
 {
     public class WoodcarverCreateViewModel
     {
-        [Required]
-        [Column(TypeName = "NVARCHAR(20)")]
+        [Required(ErrorMessage = "Please enter the first name of the woodcarver")]
+        [StringLength(NameMaxLength, ErrorMessage = $"Woodcarver first name cannot exceed 50 characters")]
         public string FirstName { get; set; } = null!;
 
-        [Required]
-        [Column(TypeName = "NVARCHAR(20)")]
+        [Required(ErrorMessage = "Please enter the last name of the woodcarver")]
+        [StringLength(NameMaxLength, ErrorMessage = $"Woodcarver last name cannot exceed 50 characters")]
         public string LastName { get; set; } = null!;
 
         [Required]
@@ -21,14 +22,15 @@ namespace WoodcarvingApp.Web.ViewModels.Woodcarver
         [BindNever]
         public SelectList CityList { get; set; } = null!;
 
-        [Required]
-        [Range(1, 120)]
+        [Required(ErrorMessage = "Please enter the phone number of the woodcarver")]
+        [Range(1, 120, ErrorMessage = "Come on man, we all know that's not the right age")]
         public int Age { get; set; }
 
         [Required]
-        [RegularExpression(@"^\+(\d{1,3})[-.\s]?(\d{1,4}[-.\s]?)*\d{4,15}$")]
+        [RegularExpression(PhoneNumberRegex, ErrorMessage = "Phone Number must with a valid country code and not more than 12 digits. Example correct format(Without the spaces): '+359 88 275 2840'")]
         public string PhoneNumber { get; set; } = null!;
 
+        [StringLength(UrlMaxLength, ErrorMessage = "Image URL cannot exceed 255 characters")]
         public string? ImageUrl { get; set; }
     }
 }
