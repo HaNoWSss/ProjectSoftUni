85b5cd3a5742edd28edd24d6ecd8d43b72c6648c
HaNoWSss <83452795+HaNoWSss@users.noreply.github.com>
2024-12-11 23:42:52 +0200

    AddedServicesToProgram&AppBinObjFiles

--- WoodcarvingApp.Web/WoodcarvingApp.Web/Program.cs
+++ WoodcarvingApp.Web/WoodcarvingApp.Web/Program.cs
@@ -1,84 +1,92 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WoodcarvingApp.Data.Models;
+using WoodcarvingApp.Services.Mapping;
 using WoodcarvingApp.Web.Data;
+using WoodcarvingApp.Web.Models;
 
 namespace WoodcarvingApp.Web
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-			string connectionString = builder.Configuration.GetConnectionString("SQLServer")!;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            string connectionString = builder.Configuration.GetConnectionString("SQLServer")!;
 
-			// Add services to the container.
-			builder.Services.AddDbContext<WoodcarvingDbContext>(options =>
-			{
-				options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer"));
-			});
-
-
-			builder.Services//
-				.AddIdentity<ApplicationUser, IdentityRole<Guid>>(cfg =>
-				{
-					ConfigureIdentity(builder, cfg);
-				})
-				.AddEntityFrameworkStores<WoodcarvingDbContext>()
-				.AddRoles<IdentityRole<Guid>>()
-				.AddSignInManager<SignInManager<ApplicationUser>>()
-				.AddUserManager<UserManager<ApplicationUser>>()
-				.AddDefaultTokenProviders();
-
-			builder.Services.ConfigureApplicationCookie(cfg =>//
-			{
-				cfg.LoginPath = "/Identity/Account/Login";
-			});
-
-			builder.Services.AddControllersWithViews();
-			builder.Services.AddRazorPages(); //
-
-			WebApplication app = builder.Build();
-
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
-
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-
-			app.UseRouting();
-
-			app.UseAuthentication();
-			app.UseAuthorization();
-
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
-			app.MapRazorPages();
-
-			app.Run();
-		}
+            // Add services to the container.
+            builder.Services.AddDbContext<WoodcarvingDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServer"));
+            });
 
 
-		private static void ConfigureIdentity(WebApplicationBuilder builder, IdentityOptions cfg)
-		{
-			cfg.Password.RequireDigit = builder.Configuration.GetValue<bool>("Identity:Password:RequireDigits");
-			cfg.Password.RequireLowercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireLowercase");
-			cfg.Password.RequireUppercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireUppercase");
-			cfg.Password.RequireNonAlphanumeric = builder.Configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumerical");
-			cfg.Password.RequiredLength = builder.Configuration.GetValue<int>("Identity:Password:RequiredLength");
-			cfg.Password.RequiredUniqueChars = builder.Configuration.GetValue<int>("Identity:Password:RequiredUniqueCharacters");
+            builder.Services//
+                .AddIdentity<ApplicationUser, IdentityRole<Guid>>(cfg =>
+                {
+                    ConfigureIdentity(builder, cfg);
+                })
+                .AddEntityFrameworkStores<WoodcarvingDbContext>()
+                .AddRoles<IdentityRole<Guid>>()
+                .AddSignInManager<SignInManager<ApplicationUser>>()
+                .AddUserManager<UserManager<ApplicationUser>>()
+                .AddDefaultTokenProviders();
 
-			cfg.SignIn.RequireConfirmedAccount = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireDigits");
-			cfg.SignIn.RequireConfirmedEmail = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedEmail");
-			cfg.SignIn.RequireConfirmedPhoneNumber = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedPhoneNumber");
+            builder.Services.ConfigureApplicationCookie(cfg =>//
+            {
+                cfg.LoginPath = "/Identity/Account/Login";
+            });
 
-			cfg.User.RequireUniqueEmail = builder.Configuration.GetValue<bool>("Identity:User:RequireUniqueEmail");
-		}
-	}
+            //builder.Services.RegisterRepositories(typeof(ApplicationUser).Assembly);
+
+            //builder.Services.AddScoped<IWoodcarvingService, IWoodcarvingService>();
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages(); //
+
+            WebApplication app = builder.Build();
+
+            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).Assembly);
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+
+            app.Run();
+        }
+
+
+        private static void ConfigureIdentity(WebApplicationBuilder builder, IdentityOptions cfg)
+        {
+            cfg.Password.RequireDigit = builder.Configuration.GetValue<bool>("Identity:Password:RequireDigits");
+            cfg.Password.RequireLowercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireLowercase");
+            cfg.Password.RequireUppercase = builder.Configuration.GetValue<bool>("Identity:Password:RequireUppercase");
+            cfg.Password.RequireNonAlphanumeric = builder.Configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumerical");
+            cfg.Password.RequiredLength = builder.Configuration.GetValue<int>("Identity:Password:RequiredLength");
+            cfg.Password.RequiredUniqueChars = builder.Configuration.GetValue<int>("Identity:Password:RequiredUniqueCharacters");
+
+            cfg.SignIn.RequireConfirmedAccount = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireDigits");
+            cfg.SignIn.RequireConfirmedEmail = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedEmail");
+            cfg.SignIn.RequireConfirmedPhoneNumber = builder.Configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedPhoneNumber");
+
+            cfg.User.RequireUniqueEmail = builder.Configuration.GetValue<bool>("Identity:User:RequireUniqueEmail");
+        }
+    }
 }
